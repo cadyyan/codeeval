bb4909c0298f91febd43e0e2d8bb2d7758ee5bc2
James Durand <james.durand@corvisa.com>
2014-03-04 11:09:10 -0600

    Fix stupid indent
    Signed-off-by: James Durand

--- python/31_rightmost.py
+++ python/31_rightmost.py
@@ -1,11 +1,11 @@
 import sys
 
 with open(sys.argv[1], 'r') as fh:
-	for line in fh.readlines():
-		line = line.strip()
-		if line == '':
-			continue
+    for line in fh.readlines():
+        line = line.strip()
+        if line == '':
+            continue
 
-		(s, c) = line.split(',')
-		print s.rfind(c)
+        (s, c) = line.split(',')
+        print s.rfind(c)
 
