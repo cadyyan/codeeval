453c28a03e25214e4966387d638e978b894dfeaa
James Durand <james.durand@corvisa.com>
2014-03-10 11:46:45 -0500

    Add c template

---
+++ c/template.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		printf("No input file given");
+		return 0;
+	}
+
+	FILE *pfh;
+	pfh = fopen(argv[1], "r");
+
+	char pcLine[1024];
+	while (fgets(pcLine, 1024, pfh) != NULL)
+	{
+		// TODO:
+	}
+
+	fclose(pfh);
+
+	return 0;
+}
+
