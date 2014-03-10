ea970fa83765dd3accaf34fb1f3f1936242413cc
James Durand <james.durand@corvisa.com>
2014-03-10 11:47:03 -0500

    Add cpp template

---
+++ cpp/template.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "No input file given" << std::endl;
+		return 0;
+	}
+
+	std::ifstream fsInput(argv[1]);
+	std::string sLine;
+	while (getline(fsInput, sLine))
+	{
+		// TODO:
+	}
+
+	fsInput.close();
+
+	return 0;
+}
+
