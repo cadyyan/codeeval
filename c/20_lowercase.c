0c0cdd166e8a4767f6101790cea9a4063ffa77d8
James Durand <james.durand@corvisa.com>
2014-03-14 07:03:36 -0500

    Add c, cpp, java, javascript solutions to challenge 20

---
+++ c/20_lowercase.c
@@ -0,0 +1,35 @@
+#include "ctype.h"
+#include "stdio.h"
+#include "string.h"
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		printf("No input file given");
+		return 0;
+	}
+
+	FILE *pfh;
+	pfh = fopen(argv[1], "r");
+
+	char pcLine[1024];
+	while (fgets(pcLine, 1024, pfh) != NULL)
+	{
+		if (!strcmp(pcLine, "\n"))
+			continue;
+
+		int nLength;
+		int i;
+	   	nLength = strlen(pcLine);
+		for (i = 0; i != nLength; i++)
+			pcLine[i] = tolower(pcLine[i]);
+
+		printf("%s", pcLine);
+	}
+
+	fclose(pfh);
+
+	return 0;
+}
+
---
+++ cpp/20_lowercase.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "No input file given" << std::endl;
+		return 0;
+	}
+
+	std::ifstream fsInput(argv[1]);
+	std::string sLine;
+	while (getline(fsInput, sLine))
+	{
+		if (sLine.empty())
+			continue;
+
+		for (std::size_t i = 0; i != sLine.length(); i++)
+			sLine[i] = tolower(sLine[i]);
+
+		std::cout << sLine << std::endl;
+	}
+
+	fsInput.close();
+
+	return 0;
+}
+
